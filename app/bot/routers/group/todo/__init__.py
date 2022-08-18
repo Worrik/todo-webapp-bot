50dc99e1154a2d9885d0bb413184b70504c29f99
Worrik <vovachornand@gmail.com>
2022-08-18 20:13:11 +0300

    :bug: fix

--- app/bot/routers/group/todo/__init__.py
+++ app/bot/routers/group/todo/__init__.py
@@ -1,8 +1,9 @@
 from aiogram.dispatcher.router import Router
-from . import base, users
+from . import base, users, tags
 
 router = Router(name="group todos")
 router.include_router(base.router)
 router.include_router(users.router)
+router.include_router(tags.router)
 
 __all__ = ("router",)
